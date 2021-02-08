6466f489f94fe4dc23bb9694f1c9e187ab6a17ee
NaomiatLibrary <ray.oshikawa@gmail.com>
2021-02-08 17:53:23 +0900

    add delete data

--- drcalculendar/app.js
+++ drcalculendar/app.js
@@ -7,9 +7,10 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var eventsRouter = require('./routes/events');
+var methodOverride = require('method-override');//PUT,DELETEを許す
 
 var app = express();
-
+app.use( methodOverride('_method') );//PUT,DELETEを許す
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
--- drcalculendar/routes/events.js
+++ drcalculendar/routes/events.js
@@ -5,15 +5,18 @@ var models = require('../models');
 /* get params*/
 router.get('/', function(req, res, next) {
   let sum=0;
+  let all_data=undefined;
   models.data.findAll({ where: { eventid: req.query.id }}).then(data => {
+    all_data=data;
     data.forEach(datum =>{
       sum+=datum.num;
     })
   })
   models.events.findByPk(req.query.id).then(event => {
-    res.render('events', {"id":req.query.id,"name":event.name,"sum":sum});
+    res.render('events', {"id":req.query.id,"name":event.name,"sum":sum,"all_data":all_data});
   }).catch(err=>{
     console.log(err);
+    res.redirect("/");
   });
   //res.json({"id":req.query.id,"message":"hello user"});
 });
@@ -30,6 +33,24 @@ router.post('/', function(req, res, next) {
     console.log(message);
   }).catch(err => {
     //失敗したらページを遷移せず警告を表示
+    console.log(err);
+  });
+  res.redirect(req.baseUrl + '?id='+String(eventid));
+});
+
+router.delete('/', function(req, res, next) {
+  let eventid=req.body.eventid;
+  let dataid=req.body.dataid;
+  console.log(eventid);
+  console.log(dataid);
+  models.data.findByPk(dataid).then(data => {
+    if(data.eventid==eventid){
+      data.destroy();
+    }else{
+      //何もしない
+      data.destroy();
+    }
+  }).catch(err=>{
     console.log(err);
   });
   res.redirect(req.baseUrl + '?id='+String(eventid));
